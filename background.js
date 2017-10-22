fbb9e56a1a67c7a87b05de3a251d3db244868d1c
Asim Zaidi <asimzaidih@gmail.com>
2017-10-22 12:59:58 -0700

    created timer

--- background.js
+++ background.js
@@ -4,27 +4,44 @@
 //   $scope.websiteList = [];
 //
 //   function addWebsite(tabId) {
+var timers = {};
     chrome.tabs.onActiveChanged.addListener(function(tabId,selectInfo) {
+      console.clear();
+      Object.keys(timers).forEach(k => {
+        console.log(`${k}: ${timers[k].totalTime}s`);
+
+      });
       chrome.tabs.get(tabId, function (tab) {
-          var url = tab.url;
+          var url = new URI(tab.url);
           var timeSpent = 0;
-          var ongoingTimer = 0;
-          var dict = {"url":url, "timer": timeSpent}
-          $('#resultslist').append("<li><span>"+dict.url+"</span></li>");
+          url = url.domain().replace(".","_");
+
+          if (!timers[url]) timers[url] = {
+            taskStart:new Date(),
+            totalTime:0
+          };
+          console.log(`Other domains: ${Object.keys(timers).filter(k => k != url).length}`);
+          Object.keys(timers)
+            //.filter(k => k != url)
+            .forEach(k=>{
+              //enumerate "other" domains and cumulate time
+              if (timers[k].taskStart) {
+                let s = (new Date() - timers[k].taskStart) / 1000;
+                timers[k].totalTime += s;
+              }
+              //start or kill timer
+              timers[k].taskStart = (k == url ? new Date() : null);
+            });
+
+
+          $('#resultslist').append("<li><span>"+timers[url]+"</span></li>");
           // $scope.websiteList.push(url);
           // console.log(url);
-          console.log(tab.title);
 
-          console.log(dict);
-
+          //Program Trace
           //Create timer
           //If url already exists in dict
-
-            //Then, timeSpent val += ongoingTimer
-
-          //Else,
-            // start ongoingTimer and save to timeSpent
-
+          //This function will run for each timer
       });
     });
   // }
